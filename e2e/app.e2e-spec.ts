632be2f2d52ba177f7315c4a814fdf1747a3a874
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-09-20 21:52:50 +0800

    changed title

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('cv App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('CV');
   });
 });
