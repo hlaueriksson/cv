2e240c4d44267a5be3760608489e53ac9d278548
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-09-27 19:19:16 +0800

    fixed app.module
    PositionsComponent was deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PositionsComponent } from './positions/positions.component';
 import { PositionComponent } from './position/position.component';
 import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PositionsComponent,
     PositionComponent,
     ProjectComponent
   ],
