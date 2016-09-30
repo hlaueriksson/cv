3553d2c4d6a76dc1564a4ec35ddcfbc458150cdb
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-09-30 15:23:49 +0800

    fixed service spec

--- src/app/resume.service.spec.ts
+++ src/app/resume.service.spec.ts
@@ -1,12 +1,24 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 import { ResumeService } from './resume.service';
 
 describe('Service: Resume', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ResumeService]
+      providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions},
+        ResumeService
+      ]
     });
   });
 
